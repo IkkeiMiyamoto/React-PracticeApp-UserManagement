cda659787e8a7d0eb6f7f94e8788336fcc3e22d3
IkkeiMiyamoto <enceladus0314@gmail.com>
2022-11-29 23:57:35 +0900

    ログイン画面を作成

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import { Box, Divider, Flex, Heading, Input, Stack } from '@chakra-ui/react';
+import React, { ChangeEvent, useState } from 'react'
 import { memo, VFC } from "react";
+import { useAuth } from '../../hooks/useAuth';
+import { PrimaryButton } from '../atoms/button/PrimaryButton';
 
 export const Login: VFC = memo(() =>{
-    return <p>ログインページです。</p>
+    const {login} = useAuth();
+    const [userId,setUserId] = useState('');
+    const onChangeUserId = (e: ChangeEvent<HTMLInputElement>) => setUserId(e.target.value);
+    return (
+        <Flex align="center" justify="center" height="100vh">
+            <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+                <Heading as="h1" size="lg" textAlign="center">ユーザー管理アプリ</Heading>
+                <Divider my={4} />
+                <Stack spacing={3} py={4} px={10}>
+                  <Input placeholder='ユーザーID' value={userId} onChange={onChangeUserId} />
+                  <PrimaryButton>ログイン</PrimaryButton>
+                </Stack>
+            </Box>
+        </Flex>
+    );
 });
