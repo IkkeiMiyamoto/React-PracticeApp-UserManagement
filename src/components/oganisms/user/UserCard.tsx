539b4976b82ff0af1731d70cd44e15d92b8a6415
IkkeiMiyamoto <enceladus0314@gmail.com>
2022-12-03 01:53:34 +0900

    ユーザー詳細画面の作成

--- src/components/oganisms/user/UserCard.tsx
+++ src/components/oganisms/user/UserCard.tsx
@@ -3,15 +3,17 @@ import React from 'react'
 import {memo,VFC} from "react";
 
 type Props = {
+    id:number;
     imageUrl:string;
     userName:string;
     fullName:string;
+    onClick: (id:number) => void;
 }
 
 export const UserCard: VFC<Props> = memo((props) =>{
-    const {imageUrl,userName,fullName} = props;
+    const {imageUrl,userName,fullName,onClick,id} = props;
     return(
-        <Box p={4} _hover={{cursor:"pointer",opacity:0.8}} w="260px" h="260px" bg="white" borderRadius="10px" shadow="md">
+        <Box p={4} _hover={{cursor:"pointer",opacity:0.8}} w="260px" h="260px" bg="white" borderRadius="10px" shadow="md" onClick={() => onClick(id)}>
             <Stack textAlign="center">
                 <Image boxSize="160px" alt={userName} m="auto" borderRadius="full" src={imageUrl}/>
                 <Text fontSize="lg" fontWeight="bold">{userName}</Text>
--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,12 +1,19 @@
-import { Center, Spinner, Wrap, WrapItem } from '@chakra-ui/react';
-import React, { useEffect } from 'react'
+import { Center,Spinner, useDisclosure, Wrap, WrapItem } from '@chakra-ui/react';
+import React, { useCallback, useEffect } from 'react'
 import { memo, VFC } from "react";
 import { useAllUsers } from '../../hooks/useAllUsers';
+import { useSelectUser } from '../../hooks/useSelectUser';
 import { UserCard } from '../oganisms/user/UserCard';
+import { UserDetailModal } from '../oganisms/user/UserDetailModal';
 
 export const UserManagement: VFC = memo(() =>{
+    const {isOpen,onOpen,onClose} = useDisclosure();
     const {getUsers,users,loading} = useAllUsers();
+    const {onSelectUser,selectedUser} = useSelectUser();
     useEffect(() => getUsers(),[getUsers]);
+    const onClickUser = useCallback((id:number) => {
+        onSelectUser({id,users,onOpen});
+    },[onOpen,onSelectUser,users]);
     return (
         <>
             {loading ? (
@@ -16,11 +23,12 @@ export const UserManagement: VFC = memo(() =>{
             <Wrap p={{base:4,md:10}}>
                 {users.map((user) =>(
                     <WrapItem key={user.id} mx="auto">
-                        <UserCard userName={user.username} fullName={user.name} imageUrl='https://images.unsplash.com/photo-1669554108285-dc5c2786ed61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2815&q=80a' />
+                        <UserCard id={user.id} userName={user.username} onClick={onClickUser} fullName={user.name} imageUrl='https://images.unsplash.com/photo-1669554108285-dc5c2786ed61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2815&q=80a' />
                     </WrapItem>
                 ))}
             </Wrap>
             )}
+            <UserDetailModal isOpen={isOpen} onClose={onClose} user={selectedUser} />
         </>
     );
 });
